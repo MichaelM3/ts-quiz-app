32adb1de3180b07add7f8e064a8a2a6988adf053
MichaelM3 <michael.muniz94@gmail.com>
2023-01-15 00:31:25 -0500

    added custom wiggle animation in tailwindconfig

--- src/components/Choice.tsx
+++ src/components/Choice.tsx
@@ -4,7 +4,7 @@ import { IChoiceProps } from '../interfaces'
 
 const Choice: FC<IChoiceProps> = ({ choice }) => {
     return (
-        <div className='flex justify-around border-2 border-solid border-black w-full p-2 rounded-2xl hover:bg-white hover:cursor-pointer'>
+        <div className='flex justify-around border-2 border-solid border-black w-full p-2 rounded-2xl hover:bg-white hover:cursor-pointer hover:animate-wiggle'>
             <span className='text-black text-4xl font-bold font-display'>
                 {choice}
             </span>
