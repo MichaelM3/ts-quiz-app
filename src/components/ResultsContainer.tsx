a47f35a00af28dcfd29271df78acb49e7282ebe8
MichaelM3 <michael.muniz94@gmail.com>
2023-01-18 16:30:44 -0500

    added more css to results container and show remaining questions

--- src/components/ResultsContainer.tsx
+++ src/components/ResultsContainer.tsx
@@ -1,20 +1,16 @@
 import { IResultsContainerProps } from '../interfaces/props'
 
-<<<<<<< HEAD
 const ResultsContainer = ({ score, currentQuestion }: IResultsContainerProps) => {
-  return (
-    <div className='border-l-2 p-4 w-1/3'>
-       <span>{score}</span>
-    </div>
-  )
-=======
-const ResultsContainer: FC<IResultsContainerProps> = ({ score, currentQuestion }) => {
     return (
-        <div className='border-l-2 p-4 w-1/3'>
-            <h2 className='text-6xl font-bold'>Score: {score}/20</h2>
+        <div className='border-l-2 p-4 w-1/3 space-y-4'>
+            <h2 className='text-6xl font-bold text-center'>Score: {score}</h2>
+            <hr />
+            <div className='flex flex-col items-center'>
+                <h2 className='text-4xl font-bold'>Questions Remaining</h2>
+                <span className='text-2xl font-bold'>{20 - (currentQuestion+1)}</span>
+            </div>
         </div>
     )
->>>>>>> e77c8081b0c475e2653116253b0d64943bf2fcf8
 }
 
 export default ResultsContainer
