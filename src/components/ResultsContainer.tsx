cabc6a196bbac685623e610dde38885bd09e1398
MichaelM3 <michael.muniz94@gmail.com>
2023-01-17 21:06:42 -0500

    added h2 for score

--- src/components/ResultsContainer.tsx
+++ src/components/ResultsContainer.tsx
@@ -2,11 +2,11 @@ import { FC } from 'react'
 import { IResultsContainerProps } from '../interfaces'
 
 const ResultsContainer: FC<IResultsContainerProps> = ({ score, currentQuestion }) => {
-  return (
-    <div className='border-l-2 p-4 w-1/3'>
-       <span>{score}</span>
-    </div>
-  )
+    return (
+        <div className='border-l-2 p-4 w-1/3'>
+            <h2 className='text-6xl font-bold'>Score: {score}/20</h2>
+        </div>
+    )
 }
 
 export default ResultsContainer
